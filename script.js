2b6c82738c8b8ea9d5b3abd2700207f1991f136d
Alexander <mrbeachcode@gmail.com>
2019-09-14 20:25:40 -0400

    made a function to display the aboutUs

--- script.js
+++ script.js
@@ -43,3 +43,19 @@ function updateDisplay (e) {
 function updateDisplay2 (e) {
   document.getElementById('logo').innerHTML = '<i class="fas fa-bolt"></i>';
 }
+
+function aboutUsChanges (e) {
+  document.getElementById('landingDisplay').style.display = 'none';
+  document.getElementById('signUpForm').style.display = 'none';
+  document.getElementById('footSignUp').style.display = 'none';
+  document.getElementById('createPost').style.display = 'none';
+  document.getElementById('userFunctions').style.display = 'none';
+  document.getElementById('actionSuccessful').style.display = 'none';
+  document.getElementById('actionError').style.display = 'none';
+  document.getElementById('viewUserPosts').style.display = 'none';
+  document.getElementById('viewAllPosts').style.display = 'none';
+  document.getElementById('viewUserComments').style.display = 'none';
+  document.getElementById('aboutElectricShadows').style.display = 'block';
+  document.getElementById('createProfile').style.display = "none";
+  document.getElementById('viewProfilePage').style.display = 'none';
+}
