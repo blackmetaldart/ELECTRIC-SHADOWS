6b5d04f7e2b47274c21c0dbd0a37892c9aec2530
Alexander <mrbeachcode@gmail.com>
2019-09-14 20:36:15 -0400

    added a function to display the function

--- script.js
+++ script.js
@@ -59,3 +59,19 @@ function aboutUsChanges (e) {
   document.getElementById('createProfile').style.display = "none";
   document.getElementById('viewProfilePage').style.display = 'none';
 }
+
+function createProfileChanges (e) {
+  document.getElementById('landingDisplay').style.display = 'none';
+  document.getElementById('signUpForm').style.display = 'none';
+  document.getElementById('footSignUp').style.display = 'block';
+  document.getElementById('createPost').style.display = 'none';
+  document.getElementById('userFunctions').style.display = 'none';
+  document.getElementById('actionSuccessful').style.display = 'none';
+  document.getElementById('actionError').style.display = 'none';
+  document.getElementById('viewUserPosts').style.display = 'none';
+  document.getElementById('viewAllPosts').style.display = 'none';
+  document.getElementById('viewUserComments').style.display = 'none';
+  document.getElementById('aboutElectricShadows').style.display = 'none';
+  document.getElementById('createProfile').style.display = "block";
+  document.getElementById('viewProfilePage').style.display = 'none';
+}
