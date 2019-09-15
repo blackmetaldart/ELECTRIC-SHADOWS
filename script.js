7d841e967cb55a615666bb3f5080762eb99661c9
Alexander <mrbeachcode@gmail.com>
2019-09-14 20:48:24 -0400

    added a function to display the updateProfile div

--- script.js
+++ script.js
@@ -209,3 +209,20 @@ function getProfileChanges (e) {
   document.getElementById('createProfile').style.display = "none";
   document.getElementById('viewProfilePage').style.display = 'block';
 }
+
+function updateProfileChanges () {
+  document.getElementById('landingDisplay').style.display = 'none';
+  document.getElementById('signUpForm').style.display = 'none';
+  document.getElementById('footSignUp').style.display = 'none';
+  document.getElementById('createPost').style.display = 'none';
+  document.getElementById('userFunctions').style.display = 'none';
+  document.getElementById('actionSuccessful').style.display = 'none';
+  document.getElementById('actionError').style.display = 'none';
+  document.getElementById('viewUserPosts').style.display = 'none';
+  document.getElementById('viewAllPosts').style.display = 'none';
+  document.getElementById('viewUserComments').style.display = 'none';
+  document.getElementById('aboutElectricShadows').style.display = 'none';
+  document.getElementById('createProfile').style.display = "none";
+  document.getElementById('viewProfilePage').style.display = 'none';
+  document.getElementById('updateProfile').style.display = 'block';
+}
