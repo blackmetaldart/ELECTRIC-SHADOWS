df6b0e0c8c7543876fa1dd596199e577b7aeb083
Alexander <mrbeachcode@gmail.com>
2019-11-09 19:38:48 -0500

    removed unnecessary code

--- BACK-END/projecttwobase/src/main/java/com/example/projecttwobase/controller/UserController.java
+++ BACK-END/projecttwobase/src/main/java/com/example/projecttwobase/controller/UserController.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class UserController {
 
